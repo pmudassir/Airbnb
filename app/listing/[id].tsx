7cbc9d6f6a7daf1c9ef8431f06c8426a4ded2cf8
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-16 14:01:47 +0530

    working on single list page

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,14 +1,37 @@
 import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import listingsData from "@/assets/data/airbnb-listings.json";
+import Animated from "react-native-reanimated";
+
+const { width } = Dimensions.get("window");
 
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+  const listing: Listing = (listingsData as any[]).find(
+    (item) => item.id === id
+  );
 
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <Animated.ScrollView>
+        <Animated.Image
+          source={{ uri: listing.xl_picture_url }}
+          style={styles.image}
+        />
+      </Animated.ScrollView>
     </View>
   );
 };
 
 export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    height: 300,
+    width,
+  },
+});
