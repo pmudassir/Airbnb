9baed57aff6581277ccbb70ed19ad812c8724b07
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-17 21:24:12 +0530

    working on single list page 2

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,15 +1,23 @@
 import { useLocalSearchParams } from "expo-router";
-import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import listingsData from "@/assets/data/airbnb-listings.json";
-import Animated from "react-native-reanimated";
+import Animated, { SlideInDown } from "react-native-reanimated";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
 
 const { width } = Dimensions.get("window");
 
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const listing: Listing = (listingsData as any[]).find(
-    (item) => item.id === id
-  );
+  const listing = (listingsData as any[]).find((item) => item.id === id);
 
   return (
     <View style={styles.container}>
@@ -18,7 +26,57 @@ const Page = () => {
           source={{ uri: listing.xl_picture_url }}
           style={styles.image}
         />
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{listing.name}</Text>
+          <Text style={styles.location}>
+            {listing.room_type} in {listing.smart_location}
+          </Text>
+          <Text style={styles.rooms}>
+            {listing.guests_included} guests · {listing.bedrooms} bedrooms ·{" "}
+            {listing.beds} bed · {listing.bathrooms} bathrooms
+          </Text>
+          <View style={{ flexDirection: "row", gap: 4 }}>
+            <Ionicons name="star" size={16} />
+            <Text style={styles.ratings}>
+              {listing.review_scores_rating / 20} · {listing.number_of_reviews}{" "}
+              reviews
+            </Text>
+          </View>
+          <View style={styles.divider} />
+          <View style={styles.hostView}>
+            <Image
+              source={{ uri: listing.host_picture_url }}
+              style={styles.host}
+            />
+            <View>
+              <Text style={{ fontWeight: "500", fontSize: 16 }}>
+                Hosted by {listing.host_name}
+              </Text>
+              <Text>Host since {listing.host_since}</Text>
+            </View>
+          </View>
+          <View style={styles.divider} />
+          <Text style={styles.description}>{listing.description}</Text>
+        </View>
       </Animated.ScrollView>
+      <Animated.View
+        style={defaultStyles.footer}
+        entering={SlideInDown.delay(200)}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+          <TouchableOpacity style={styles.footerText}>
+            <Text style={styles.footerPrice}>€ {listing.price}</Text>
+            <Text>night</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[defaultStyles.btn, { padding: 16 }]}>
+            <Text style={defaultStyles.btnText}>Reserve</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
     </View>
   );
 };
@@ -28,10 +86,87 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
   },
   image: {
     height: 300,
     width,
+  },
+  infoContainer: {
+    padding: 24,
+    backgroundColor: "#fff",
+  },
+  name: {
+    fontSize: 26,
+    fontWeight: "bold",
+    fontFamily: "mon-sb",
+  },
+  location: {
+    fontSize: 18,
+    marginTop: 10,
+    fontFamily: "mon-sb",
+  },
+  rooms: {
+    fontSize: 16,
+    color: Colors.grey,
+    marginVertical: 4,
+    fontFamily: "mon",
+  },
+  ratings: {
+    fontSize: 16,
+    fontFamily: "mon-sb",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.grey,
+    marginVertical: 16,
+  },
+  host: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  hostView: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  footerText: {
+    height: "100%",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  footerPrice: {
+    fontSize: 18,
+    fontFamily: "mon-sb",
+  },
+  roundButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    color: Colors.primary,
+  },
+  bar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  header: {
+    backgroundColor: "#fff",
+    height: 100,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.grey,
+  },
+  description: {
+    fontSize: 16,
+    marginTop: 10,
+    fontFamily: "mon",
   },
 });
